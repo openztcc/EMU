1785139e256ec54a77b2c02bea9a7ddf9226d92a
Eric Galvan <lericgalvan@outlook.com>
2024-03-19 23:35:33 -0700

    updated readme  with latest project requirements, documentation links

---
+++ README.md
@@ -0,0 +1,28 @@
+<h1 align="center">EMU | Extensible Modding Utility</h1>
+
+<h3 align="center"> A Zoo Tycoon 1 Scripting API for Modders</h3>
+
+Vanilla Zoo Tycoon 1 has over a 20 year history of modding but users have only been able to modify the game through editing of INI configuration files. This project enhances modding capabilities by giving modders Lua scripting support so that they can create more complex and interesting mods.
+
+### Compatibility
+
+To use this API, you must:
+
+- Own the Zoo Tycoon Complete Collection, or the base game with both expansions installed.
+- Have a Windows 7 or newer operating system.
+- Install the [Microsoft Visual C++ 2017 Redistributable](https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170).
+
+### Build from Source
+
+The .sln file has been included for the option to build from source. The project is build with Visual Studio 2022.
+
+### Installation
+
+Drop the res-EMU.dll binary directly into your `C:\Program Files (x86)\Microsoft Games\Zoo Tycoon` game directory.
+
+### Documentation
+
+Reference documentation for the the debug console and API can be found at the Zooberry Docs reference section: [EMU API Reference](https://docs.zooberry.org/zt1/reference/emu/).
+
+For examples and tutorials on how to use Lua scripting with Zoo Tyccon 1, visit the Zooberry Docs tutorial section: [EMU Tutorials](https://docs.zooberry.org/zt1/advanced/).
+
--- reimp/ZTGameMgr.cpp
+++ reimp/ZTGameMgr.cpp
@@ -29,56 +29,6 @@ void ZTGameMgr::init() {
     // DetourAttach((PVOID*)&setZooRatingAddress, (PVOID)&SetZooRating);
     // DetourAttach((PVOID*)&setGuestRatingAddress, (PVOID)&SetGuestRating);
     DetourTransactionCommit();
-    // DWORD _ztGameMgr = *(DWORD*)((LPVOID)0x00638048);
-    // ZTGameMgr::shared_instance() = *(ZTGameMgr*)(_ztGameMgr);
-
-    // EmuBase::callHook(0x0050A245, (DWORD)&ZTGameMgr::addCash_Detour); // from removeEntityFromMap
-    // EmuBase::callHook(0x00613f61, (DWORD)&ZTGameMgr::addCash_Detour); // from ZooStatus::fGrantDonation
-    // EmuBase::callHook(0x0060d9d3, (DWORD)&ZTGameMgr::addCash_Detour); // from unknown fn at 0060d8e3
-    // EmuBase::callHook(0x0060cccd, (DWORD)&ZTGameMgr::addCash_Detour); // from unknown fn at 433ae0
-    // EmuBase::callHook(0x00613f61, (DWORD)&ZTGameMgr::addCash_Detour); // from unknown fn at 433b00
-    // EmuBase::callHook(0x006089fc, (DWORD)&ZTGameMgr::addCash_Detour); // from unknown fn at cls_0x40f018::meth_0x6089e8
-    // EmuBase::callHook(0x00607299, (DWORD)&ZTGameMgr::addCash_Detour); // from georgeW
-    // EmuBase::callHook(0x0060d9d3, (DWORD)&ZTGameMgr::addCash_Detour); // from unknown fn at 0060d8e3
-    // EmuBase::callHook(0x005b0f2d, (DWORD)&ZTGameMgr::addCash_Detour); // from unknown fn at 005b0f17
-    // EmuBase::callHook(0x005a1679, (DWORD)&ZTGameMgr::addCash_Detour); // from ZTScenarioSimpleGoal::trigger03
-    // EmuBase::callHook(0x004f7031, (DWORD)&ZTGameMgr::addCash_Detour); // from unknown fn at cls_0x41f881::meth_0x4f6e3c
-    // EmuBase::callHook(0x004d920f , (DWORD)&ZTGameMgr::addCash_Detour); // from ZTMapView::placeEntityOnMap
-    // EmuBase::callHook(0x004a2f97, (DWORD)&ZTGameMgr::addCash_Detour); // from ZTGameMgr::removedZooDoo
-    // EmuBase::callHook(0x00484047, (DWORD)&ZTGameMgr::addCash_Detour); // from ZooStatus::financeChecks
-    // EmuBase::callHook(0x0042ec81, (DWORD)&ZTGameMgr::addCash_Detour); // from ZTHabitat::acceptDonation
-    // EmuBase::callHook(0x0042d93f, (DWORD)&ZTGameMgr::addCash_Detour); // from ZTBuilding::removeUser
-    // EmuBase::callHook(0x005a981c, (DWORD)&ZTGameMgr::addCash_Detour); // from ZTBuilding::addUser
-
-    // EmuBase::callHook(0x004cb64f, (DWORD)&ZTGameMgr::setCash_Detour); // from showMapSelect
-    // EmuBase::callHook(0x006163ab, (DWORD)&ZTGameMgr::setCash_Detour); // from clickCashUp
-    // EmuBase::callHook(0x0061640d, (DWORD)&ZTGameMgr::setCash_Detour); // from clickCashDown
-    // EmuBase::callHook(0x006165ce, (DWORD)&ZTGameMgr::setCash_Detour); // from editStartingCash
-
-
-
-    // EmuBase::callHook(0x0041f284, (DWORD)&ZTGameMgr::subtractCash_Detour); // from ZTResearchBranch::update <--- crashes, not sure why
-    // EmuBase::callHook(0x0041f304, (DWORD)&ZTGameMgr::subtractCash_Detour); // from unknown fn at 0041f304 <--- crashes, not sure why
-    // // EmuBase::callHook(0x00469281, (DWORD)&ZTGameMgr::subtractCash_Detour); // from unknown fn at 00469281
-    // // EmuBase::callHook(0x0046944c, (DWORD)&ZTGameMgr::subtractCash_Detour); // from unknown fn at 0046944c
-    // // EmuBase::callHook(0x004702e3, (DWORD)&ZTGameMgr::subtractCash_Detour); // from unknown fn at 004702e3
-    // // EmuBase::callHook(0x0047c043, (DWORD)&ZTGameMgr::subtractCash_Detour); // from unknown fn at 0047c043
-    // // EmuBase::callHook(0x004841a3, (DWORD)&ZTGameMgr::subtractCash_Detour); // from financeChecks
-    // // EmuBase::callHook(0x00484230, (DWORD)&ZTGameMgr::subtractCash_Detour); // from financeChecks
-    // // EmuBase::callHook(0x00488e9d, (DWORD)&ZTGameMgr::subtractCash_Detour); // from fn at virt_meth_0x4da252:00488e9d
-    // // EmuBase::callHook(0x0048a021, (DWORD)&ZTGameMgr::subtractCash_Detour); // from virt_meth_0x489fcd
-    // // EmuBase::callHook(0x0048f76c, (DWORD)&ZTGameMgr::subtractCash_Detour); // from virt_meth_0x48f624
-    // // EmuBase::callHook(0x0049befe, (DWORD)&ZTGameMgr::subtractCash_Detour); // from meth_0x49bec7
-    // // EmuBase::callHook(0x004a099a, (DWORD)&ZTGameMgr::subtractCash_Detour); // from removeEntityFromMap
-    // // EmuBase::callHook(0x004d8eda, (DWORD)&ZTGameMgr::subtractCash_Detour); // from placeEntityOnMap
-    // // EmuBase::callHook(0x004deb1b, (DWORD)&ZTGameMgr::subtractCash_Detour); // from FUN_005b6710
-    // // EmuBase::callHook(0x004efd66, (DWORD)&ZTGameMgr::subtractCash_Detour); // from removeEntityFromMap
-    // // EmuBase::callHook(0x005ce004, (DWORD)&ZTGameMgr::subtractCash_Detour); // from removeEntityFromMap
-    // // EmuBase::callHook(0x005ce042, (DWORD)&ZTGameMgr::subtractCash_Detour); // from removeEntityFromMap
-    // // EmuBase::callHook(0x005ce068, (DWORD)&ZTGameMgr::subtractCash_Detour); // from removeEntityFromMap
-    // // EmuBase::callHook(0x00611666, (DWORD)&ZTGameMgr::subtractCash_Detour); // from FUN_006115f2
-
-
 }
 
 // void ZTGameMgr::addCash(float amount) {
@@ -103,33 +53,6 @@ DWORD ZTGameMgr::getZTGameMgr() {
     // get the instance of ZTGameMgr
     DWORD _ztGameMgr = *(DWORD*)((LPVOID)0x00638048);
     return *(DWORD*)(_ztGameMgr);
-}
-
-_FILETIME* ZTGameMgr::getDate_Detour(void* ptr, _FILETIME* date) {
-    // detour function for getting the current date from the game
-    return ZTGameMgr::shared_instance().getDate(date);
-}
-
-// void ZTGameMgr::subtractCash_Detour(void* ptr, float amount) {
-//     // detour function for subtracting cash from the game
-//     ZTGameMgr::shared_instance().subtractCash(amount);
-// }
-
-void ZTGameMgr::setCash_Detour(int amount) {
-    // detour function for setting cash in the game
-    // Log the amount of cash being set
-    std::ofstream logfile("cash.txt", std::ios::app);
-    if (logfile.is_open()) {
-        logfile << "Setting cash to: " << amount << std::endl;
-        logfile.close();
-    }
-
-    ZTGameMgr::shared_instance().setCash(amount);
-}
-
-void ZTGameMgr::setCash(int amount) {
-    _setCash _ogsetCash = (_setCash)0x004cb384;
-    _ogsetCash(amount);
 }
 
 // void __fastcall ZTGameMgr::addCash_Detour(void* ptr, float amount) {
